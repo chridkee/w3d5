c164578752cb3c07b09ba1d98aeed8bd62cbe42b
chridkee <christopher.keesor@gmil.com>
2023-01-27 15:32:24 -0500

    parts 1-3

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -6,24 +6,49 @@ class PolyTreeNode
         @children = []
         @value = value
     end
-
+    #             #node1
+    #             #node2.parent
+#     def parent=(parent_node)
+#         if parent_node == nil
+#             @parent = nil 
+#         else
+#             #true or false 
+#             unique = parent_node.children.all? {|el| el.value != self.value}
+#                     #node1.chidren [node2, node3].all?
+#             if unique                   #node1
+#                 parent_node.children << self 
+#             end
+#             @parent = parent_node
+#         end
+#     end
+# end
     def parent=(parent_node)
-        # if self.parent != nil
-            @parent.children.delete(self) # remove from parent's children array
+        
+        @parent.children.delete(self) if self.parent != nil # remove from parent's children array
            
+        if parent_node == nil 
             @parent = nil # set parent to nil
-        # else
-            unique = parent_node.children.all? {|el| el.value != self.value}
+        else
+        unique = parent_node.children.all? {|el| el.value != self.value}
             if unique 
                 parent_node.children << self 
             end
             @parent = parent_node
-        # end
-    end 
+        end 
+    end
       
     def inspect 
         @value.inspect 
     end 
+
+    def add_child(child_node)
+        child_node.parent = self
+    end 
+
+    def remove_child(child_node)
+        child_node.parent = nil 
+        child_node.parent.children.delete(child_node)
+    end
     
 end
 
@@ -36,3 +61,6 @@ end
 
         # a self: children: [], parent: b
         # b parent: children[a], parent
+
+
+        
