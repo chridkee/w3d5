694fb4f13eb5cda9efecdef4bca654731dc40814
chridkee <christopher.keesor@gmil.com>
2023-01-27 14:33:48 -0500

    2:33 update

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -7,12 +7,27 @@ class PolyTreeNode
         @value = value
     end
 
-    def parent=(parent)
-        return parent if parent == nil
-        @parent = parent
-        if !@children.include?(self)
-            parent.children << self
+    def parent=(parent_node)
+        if parent_node == nil
+            return nil 
+        else
+            unique = parent_node.children.all? {|el| el.value != self.value}
+            if unique 
+                parent_node.children << self 
+            end
+            @parent = parent_node
         end
-    end
+    end 
+      
+    def inspect 
+        @value.inspect 
+    end 
     
 end
+
+  # each do |el|
+        #     if el.value == self.value 
+        # end 
+        # if !@children.include?(self)
+        #     parent.children << self
+        # end
